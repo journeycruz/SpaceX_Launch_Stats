5a745761356c7d5cf2252496e11a0a8e673e893d
Journey Cruz <journey.cruz@gmail.com>
2021-07-29 21:57:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require('express');
 var { graphqlHTTP } = require('express-graphql');
-// const cors = require('cors');
 const schema = require('./schema');
-// const path = require('path');
 
 const app = express();
-
-// Allow cross-origin
-// app.use(cors());
 
 app.use(
   '/graphql',
@@ -16,12 +11,6 @@ app.use(
     graphiql: true
   })
 );
-
-// app.use(express.static('public'));
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-// });
 
 const PORT = process.env.PORT || 5000;
 
