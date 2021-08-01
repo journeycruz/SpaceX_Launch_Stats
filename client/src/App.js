21e9eb23b5f5034cfb1fa9f01b7afe03b1dc636b
Journey Cruz <journey.cruz@gmail.com>
2021-08-01 12:01:33 -0500

    Installed React-Router to route to pages and set path for "/" and for "/launch/:flight_number" to route to second page if user requests more info about specific rocket

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './SpaceXLogo.png';
 import Launches from './components/Launches';
+import Launch from './components/Launch';
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql'
@@ -11,10 +13,14 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client} >
+      <Router>
       <div className="container">
         <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
-        <Launches />
+        <Route exact path="/" component={Launches} />
+        <Route exact path="/launch/:flight_number" component={Launch} />
+
       </div>
+      </Router>
   </ApolloProvider>
   );
 }
---
+++ client/src/components/Launch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Launch extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Launch</h1>
+            </div>
+        )
+    }
+}
+
+export default Launch;
