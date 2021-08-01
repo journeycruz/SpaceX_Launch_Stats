9a9f69f46a4b604d51254377b032ef18652ed270
Journey Cruz <journey.cruz@gmail.com>
2021-08-01 00:06:44 -0500

    Enabled Cors in server for cross-origin. Created a query for Launches to start rendering to page

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,21 @@
 import './App.css';
 import ApolloClient from 'apollo-boost';
-import {
-  ApolloProvider
-} from 'react-apollo';
+import { ApolloProvider } from 'react-apollo';
 import logo from './SpaceXLogo.png';
+import Launches from './components/Launches';
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql'
 });
 
 function App() {
-  return ( <
-    ApolloProvider client = {
-      client
-    } >
-    <
-    div className = "Container" >
-    <
-    img src = {
-      logo
-    }
-    alt = "SpaceX"
-    style = {
-      {
-        width: 300,
-        display: 'block',
-        margin: 'auto'
-      }
-    }
-    /> <
-    /div> <
-    /ApolloProvider>
+  return (
+    <ApolloProvider client={client} >
+      <div className="container">
+        <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
+        <Launches />
+      </div>
+  </ApolloProvider>
   );
 }
 
